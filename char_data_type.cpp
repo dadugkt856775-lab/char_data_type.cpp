2fee27fcaede469b0b3cca48f3b53f5392e400b2
Dadakhalandar S A <dadugkt856775@gmail.com>
2026-05-28 16:11:22 +0530

    Add files via upload

---
+++ char_data_type.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char x = 'e';
+    char ch = 't';
+    char y = 'j';
+    char z = 'u';
+    char m = 's';
+    cout<<ch<<endl<<x<<endl;
+    cout<<y<<endl<<z<<endl<<m;
+}
